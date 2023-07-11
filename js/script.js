8d02076e60a29d1366a9390f7abd13d87f1faab7
nader-web <94446474+nader-web@users.noreply.github.com>
2023-07-11 18:57:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const fetchAdvice = async () => {
 	try {
 		const res = await fetch("https://api.adviceslip.com/advice");
 		const data = await res.json();
-		document.getElementById("advice-id").innerHTML = `Advice #${data.slip.id}`;
+		document.getElementById("advice-id").innerHTML = `ADVICE #${data.slip.id}`;
 		document.getElementById("advice-text").innerHTML = `"${data.slip.advice}"`;
 	} catch (err) {
 		console.error(err);
