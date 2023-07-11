2cf2e5366c95f5349dcdc2419e871f4300390a4c
nader-web <94446474+nader-web@users.noreply.github.com>
2023-07-11 18:20:37 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// function to fetch advice
+
+const fetchAdvice = async () => {
+	try {
+		const res = await fetch("https://api.adviceslip.com/advice");
+		const data = await res.json();
+		document.getElementById("advice-id").innerHTML = `Advice #${data.slip.id}`;
+		document.getElementById("advice-text").innerHTML = `"${data.slip.advice}"`;
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+// fetch advice api when page is loaded
+window.onload = () => {
+	fetchAdvice();
+};
+// when button is clicked
+
+document.getElementById("dice-btn").addEventListener("click", () => {
+	fetchAdvice();
+});
+// write a function to toggle hiding icon when mobile size is active
+ 
